f7c0b91cd62e56e835089a61ad5b041ec1426104
Huynh Quoc Huy <huynhquochuy06012005@gmail.com>
2024-01-17 16:18:23 +0700

    .

--- webbanhang/webbanhang/urls.py
+++ webbanhang/webbanhang/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls.static import static #import thư viện static
+from django.conf     import settings #import thư viện static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls'))
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
